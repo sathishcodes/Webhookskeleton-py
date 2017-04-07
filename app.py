1e8bc821fdfba90965c5a343467cfb32cce753f5
sathishcodes <pingsathish@gmail.com>
2017-04-07 18:01:58 +0530

    enclosed education to quotes

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def makeWebhookResult(req):
 
     info = {'Education':'M.Sc Software Engineering'}
 
-    speech = "His educational qualification is " +  str(info[Education]) + "."
+    speech = "His educational qualification is " +  str(info['Education']) + "."
 
     print("Response:")
     print(speech)
