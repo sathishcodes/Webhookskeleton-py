46427c1216584a68de766f267f35fde07dba6ec1
sathishcodes <pingsathish@gmail.com>
2017-05-14 19:52:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,12 +67,8 @@ def makeWebhookResult(req):
         else:
             speech = fb_str + str(feedback) + str(blob.sentiment.polarity) + "Negative"
     
-    elif action == "get.feedback.ask-feedback-fallback":
-        
-        if result.get("contexts").get("name") == "ask-feedback-followup":
-            speech = "Thanks for feedback"
-        else:
-            speech = "Sorry, I didn't get that"
+    elif action == "get.feedback.ask-feedback-fallback":                
+         speech = "Thanks for feedback"                    
         
     return {
       "speech": speech,
